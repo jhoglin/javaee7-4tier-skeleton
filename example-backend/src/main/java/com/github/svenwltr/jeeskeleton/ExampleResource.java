0c23aa5dceef5b6f97708aeb481c39efd0dfac1f
Sven Walter <sven@wltr.eu>
2014-09-20 18:28:07 +0200

    Some more meaningful example resource.

---
+++ example-backend/src/main/java/com/github/svenwltr/jeeskeleton/ExampleResource.java
@@ -0,0 +1,51 @@
+package com.github.svenwltr.jeeskeleton;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@RequestScoped
+@Path("example")
+public class ExampleResource {
+
+	@EJB
+	ExampleService example;
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Item> getAll() {
+		// playing with new streaming api
+		return example.findAll().entrySet().stream()
+				.map(e -> new Item(e.getKey(), e.getValue()))
+				.collect(Collectors.toList());
+
+	}
+
+	@POST
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Item add(String text) {
+		return new Item(example.create(text), text);
+
+	}
+
+	public static class Item {
+		public int id;
+		public String text;
+
+		public Item(int id, String text) {
+			this.id = id;
+			this.text = text;
+		}
+
+	}
+
+}
--- example-service/src/main/java/com/github/svenwltr/jeeskeleton/ExampleService.java
+++ example-service/src/main/java/com/github/svenwltr/jeeskeleton/ExampleService.java
@@ -1,51 +1,32 @@
 package com.github.svenwltr.jeeskeleton;
 
-import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
-
-import com.google.common.hash.HashFunction;
-import com.google.common.hash.Hashing;
 
 @Singleton
 @LocalBean
 public class ExampleService {
 
-	private final static HashFunction HF = Hashing.sha512();
-
 	// this should be stored in a database
 	private final Map<Integer, String> data = new HashMap<>();
+	private int pk = 1;
 
-	private int position = 0;
-
-	public String sayHello(String name) {
-		return "Hello " + name + "!";
+	public Map<Integer, String> findAll() {
+		return data;
 
 	}
 
-	public int doMagic(String name) {
-		int id = position++;
-		String hash = HF.hashString(name, Charset.forName("UTF_8")).toString();
-		data.put(id, hash);
+	public int create(String text) {
+		int id;
+		do {
+			id = pk++;
+		} while (data.containsKey(text));
 
+		data.put(id, text);
 		return id;
-
-	}
-
-	public String getStuff(int id) {
-		if (data.containsKey(id))
-			return data.get(id);
-		else
-			return null;
-
-	}
-
-	public Set<Integer> keys() {
-		return data.keySet();
 
 	}
 
